e2429d144d9b51edf17a393284c7ce1a1dca5cb8
Gretchen <gretchen.y.ma@gmail.com>
2023-08-24 01:58:19 +0800

    fix syn

--- all_func_normalization_nosyn.sh
+++ all_func_normalization_nosyn.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 echo "Begin spatial normalization"
 
-antsApplyTransforms -e 3 -d 3 -i ../func/filtered_func_data.nii.gz -o func2standard.nii.gz \
--r ../standard.nii.gz \
--t reg_a2t_0GenericAffine.mat -t reg_f2a_0GenericAffine.mat
+antsApplyTransforms -e 3 -d 3 -i ../func/filtered_func_data.nii.gz \
+-o func2standard.nii.gz \
+-r ../standard.nii.gz  \
+-t reg_a2t_0GenericAffine.mat \
+-t reg_f2a_0GenericAffine.mat
 
 
 echo "End spatial normalization"
